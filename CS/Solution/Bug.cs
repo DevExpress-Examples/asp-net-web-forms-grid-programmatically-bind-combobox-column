35b949c868d697b67fb7b2b4ad35bfc4048d1c63
EugeniyBurmistrov <psycrab88@gmail.com>
2022-09-14 17:27:35 +0400

    Update Readme and solution

--- CS/Solution/Bug.cs
+++ CS/Solution/Bug.cs
@@ -1,15 +1,6 @@
-using System;
-using System.Data;
-using System.Configuration;
-using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
-namespace BindToList {
+namespace Solution {
     public class Bug {
         private int id;
 
---
+++ CS/Solution/BugStatus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Solution {
+    public class BugStatus {
+        private int id;
+
+        public int Id {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public static List<BugStatus> GetStatusesList() {
+            string[] names = new string[] { "Accepted", "Rejected", "Fixed" };
+            List<BugStatus> result = new List<BugStatus>(names.Length);
+            for (int i = 0; i < names.Length; i++) {
+                BugStatus st = new BugStatus();
+                st.Name = names[i];
+                st.id = i;
+                result.Add(st);
+            }
+            return result;
+        }
+
+    }
+}
